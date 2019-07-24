c87c5dac0213c0fc0650bb35f3855e2694fa8a1d
Ann Hillis <andriellerhillis@gmail.com>
2019-07-24 13:01:07 -0500

    Messing with the possibility of a server

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
-import {createServer} from 'http';
-import express from 'express';
-import compression from 'compression';
-import morgan from 'morgan';
-import path from 'path';
+const {createServer} = require('http');
+const express = require('express');
+const compression = require('compression');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const dev = app.get('env') !== 'production';
+
+if(!dev){
+    app.disable('x-powered-by');
+    app.use(compression());
+    app.use(morgan('common'));
+    app.use(express.static(path.resolve(__dirname, 'build')));
+
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    })
+}
+
+if(dev){
+    app.use(morgan('dev'));
+}
+
+const server = createServer(app)
+const normalizePort = port => parseInt(port, 10)
+const PORT = normalizePort(process.env.PORT || 5000);
+
+server.listen(PORT, err =>{
+    if(err)
+        throw err
+
+    console.log(`Server running on port ${PORT}`)
+})
 
 
