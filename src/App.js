eee813b6552a5cb57e290138486c8891ef69f9d3
Andrielle Hillis <andriellerhillis@gmail.com>
2022-10-08 12:40:35 -0500

    Connected Searchbar to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 //import './App.scss';
 
+import Search from './components/search/search';
+
 class App extends React.Component {
   render(){
     return (<div className="App">
-      App goes here
+      <Search />
     </div>);
   }
 }
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
-
-class Display extends Component {
-    render() {
-        const {forcast: {currentTemp, high, low, phrase}, location: {city, state, zip}} = this.props;
-        return (
-            <article className="one-day-display">
-                <header>
-                    <h2>
-                    {city}, {state} {zip}
-                    </h2>
-                </header>
-                <section>
-                    <p className="current-temp">{currentTemp} &deg;F</p>
-                    <p>{phrase.charAt(0).toUpperCase() + phrase.slice(1)}</p>
-                </section>
-                <section>
-                    <p>High: {high} &deg;F</p>
-                    <p>Low: {low} &deg;F</p>
-                </section>
-            </article>
-        );
-    }
+const Search = () =>{
+    return ('Searchbar goes here')
 }
 
-export default Display;
+export default Search;
+// import React, { Component } from 'react';
+
+// class Display extends Component {
+//     render() {
+//         const {forcast: {currentTemp, high, low, phrase}, location: {city, state, zip}} = this.props;
+//         return (
+//             <article className="one-day-display">
+//                 <header>
+//                     <h2>
+//                     {city}, {state} {zip}
+//                     </h2>
+//                 </header>
+//                 <section>
+//                     <p className="current-temp">{currentTemp} &deg;F</p>
+//                     <p>{phrase.charAt(0).toUpperCase() + phrase.slice(1)}</p>
+//                 </section>
+//                 <section>
+//                     <p>High: {high} &deg;F</p>
+//                     <p>Low: {low} &deg;F</p>
+//                 </section>
+//             </article>
+//         );
+//     }
+// }
+
+// export default Display;
