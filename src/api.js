e5eab86ba43d7a41ab4092b915b6350aab6a0839
Andrielle Hillis <andriellerhillis@gmail.com>
2022-10-09 13:45:09 -0500

    Geo API added

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '27c05b0f3emsh70bd1997cf96a5cp1f8a0cjsn2e802a92d74d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions'
+
