7303d23d681344064cf7063ad5a1c12a7ec7786f
Andrielle Hillis <andriellerhillis@gmail.com>
2022-10-12 21:12:22 -0500

    Current location added

--- src/App.js
+++ src/App.js
@@ -28,11 +28,33 @@ function App() {
       setCurrentWeather({currentCity: searchData.label, ...weatherFormatted});
       setCurrentForecast({currentCity: searchData.label, ...forecastFormatted});
       
-    }
+  }
+
+  const getCurrentLocation = () =>{
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        console.log("Geolocation is not supported by this browser.");
+      }
+  }
+
+  function showPosition(position) {
+    //const {latitude, longitude} = position.coords;
+    const mapquest_API_KEY = "oz9OL7hgFsSS19Ljsn3iL4PNMxGRT90E";
+    //http://www.mapquestapi.com/geocoding/v1/address?key=KEY&location=
+    fetch(`http://open.mapquestapi.com/geocoding/v1/address?key=${mapquest_API_KEY}&location=De+Kalb`)
+      .then(res => res.json())
+      .then(data => console.log(data.results[0].locations))//.results[0]
+      .catch(err => console.log(err))
+
+  }
 
     return (<Container className="App">
       <Button variant="primary" onClick={showModal}>
         Change Location
+      </Button>
+      <Button variant="primary" onClick={getCurrentLocation}>
+        Get Position
       </Button>
 
       <Modal show={modalShown} onHide={hideModal}>
@@ -76,7 +98,7 @@ function App() {
 //         low: ""
 //       },
 //       openWeather_API_KEY: "87ee9d9eb500edc3fa8b18f1e1c97509",
-//       mapquest_API_KEY: "oz9OL7hgFsSS19Ljsn3iL4PNMxGRT90E"
+//       
 
 //     }
 //   }
@@ -90,7 +112,7 @@ function App() {
 //       const {name:city, coord: {lat, lon}, main:{temp, temp_max, temp_min}, weather} = data;
 //       const {description, icon} = weather[0];
 
-//       fetch(`http://open.mapquestapi.com/geocoding/v1/address?key=${mapquest_API_KEY}&location=${lat},${lon}`)
+//       
 //       .then(res => res.json())
 //       .then(data => 
 //         this.setState({
