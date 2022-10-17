aff7c86457b118a8cd9fbb0e7e2860c5cbe1b7af
Andrielle Hillis <andriellerhillis@gmail.com>
2022-10-17 14:49:30 -0500

    Got rid of Geo API

--- src/api.js
+++ src/api.js
@@ -1,13 +1,3 @@
-export const geoApiOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '27c05b0f3emsh70bd1997cf96a5cp1f8a0cjsn2e802a92d74d',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
-};
-
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions';
-
 export const MAPBOX_API_KEY = "oz9OL7hgFsSS19Ljsn3iL4PNMxGRT90E";
 
 export const MAPBOX_API_URL = "http://open.mapquestapi.com/geocoding/v1/";
