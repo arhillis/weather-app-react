24c52193b09c03eac1b5bd260dd03a18ca01ec8c
Andrielle Hillis <andriellerhillis@gmail.com>
2022-11-01 15:52:34 -0500

    Got rid of dayOfTheWeek prop

--- src/components/current-forecast/index.js
+++ src/components/current-forecast/index.js
@@ -4,13 +4,10 @@ import { Accordion} from 'react-bootstrap';//, ListGroup
 import Header from './header';
 import Body from './body';
 
-const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
                                     
 
 function CurrentForecast(props){
     const {daily} = props.currentForecast;//, hourly
-    const dayOfTheWeek = new Date().getDay();
-    const forecastDays = WEEK_DAYS.slice(dayOfTheWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayOfTheWeek));
 
     return (<div className='forecast'>
                 <label>Forecast</label>
@@ -18,7 +15,7 @@ function CurrentForecast(props){
                     {daily.slice(1).map((day, index) => (
                         <Accordion.Item eventKey={index} key={index}>
                             <Accordion.Header>
-                                <Header day={day} dayOfTheWeek={forecastDays[index]}/>
+                                <Header day={day}/>
                             </Accordion.Header>
                             <Accordion.Body>
                                 <Body day={day} />
