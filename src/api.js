1a7ac7f0b016f67fbf4a9623aa1bd2e27ac4b1f2
Andrielle Hillis <andriellerhillis@gmail.com>
2022-11-01 14:44:20 -0500

    Changed to Geo API

--- src/api.js
+++ src/api.js
@@ -1,6 +1,17 @@
-export const MAPBOX_API_KEY = "oz9OL7hgFsSS19Ljsn3iL4PNMxGRT90E";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "94e61eb50fmshd2c33340bdf75dcp139e6ajsn166080a7e870",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+  params: {countryIds: 'US'}
+};
 
-export const MAPBOX_API_URL = "http://open.mapquestapi.com/geocoding/v1/";
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const MAPBOX_API_KEY = "pk.eyJ1IjoiYXJoaWxsaXMiLCJhIjoiY2w5eTllbWxzMDJtbzN1cG0zOW1zbWsxeCJ9.ERr_X-vsKe_9t5NqbPYw6Q";
+
+export const MAPBOX_API_URL = "https://api.mapbox.com/geocoding/v5";
 
 export const WEATHER_API_KEY = "87ee9d9eb500edc3fa8b18f1e1c97509";
 
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,29 +1,31 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import { MAPBOX_API_KEY, MAPBOX_API_URL } from "../../api";
+import { GEO_API_URL, geoApiOptions } from "../../api";
 
 const Search = ({onSearchChange}) =>{
 
     const [search, setSearch] = useState(null);
 
     const loadOptions = (searchValue, loadedOptions) =>{
-        //const mapquest_API_KEY = "oz9OL7hgFsSS19Ljsn3iL4PNMxGRT90E";
-        return fetch(`${MAPBOX_API_URL}/address?key=${MAPBOX_API_KEY}&location=${searchValue}`)
-            .then(response => response.json())
-            .then((response) => {
-                return {
-                options: response.results[0].locations
-                .filter((city) => city.adminArea5.length > 0)
-                .map((city) => {
-                    const {adminArea1, adminArea3, adminArea5, displayLatLng} = city;
-                    return {
-                        value: `${displayLatLng.lat} ${displayLatLng.lng}`,
-                        label: `${adminArea5}, ${adminArea1 === 'US' ? ` ${adminArea3} (US)` : adminArea1}`
-                    };
-                }),
-                };
-            });
+        if(searchValue && searchValue !== ''){
+            const url = `${GEO_API_URL}/cities?namePrefix=${searchValue}`;
+            return fetch(url, geoApiOptions)
+                .then(response => response.json())
+                .then(res =>{
+                    const {data} = res;
+                    const options = data.map(option =>{
+                        return {
+                            value: `${option.latitude} ${option.longitude}`,
+                            label: `${option.city}, ${option.regionCode}`
+                        }
+                    })
+                    return {options: options.length > 0 ? options : []};
+                })
+                // .then(() => {return {options: []}})
+                .catch(err => console.log(err));
+        }
 
+        return {options: []}
     }
 
     const handleOnChange = (searchData) =>{
