879643fcd4292fd03985d4c76274768cb6de5846
Andrielle Hillis <andriellerhillis@gmail.com>
2022-11-03 08:05:44 -0500

    Added weather provider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import App from "./App";
+import { WeatherProvider } from "./context/weather-context";
 //import reportWebVitals from "./reportWebVitals";
 
 import { Container } from "react-bootstrap";
@@ -13,16 +14,18 @@ import DailyForecast from "./components/daily-forecast";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Menu />
-      <Container>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/test" element={<Test />} />
-          <Route path="/daily-forecast" element={<DailyForecast />} />
-        </Routes>
-      </Container>
-    </Router>    
+    <WeatherProvider>
+      <Router>
+        <Menu />
+        <Container>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/test" element={<Test />} />
+            <Route path="/daily-forecast" element={<DailyForecast />} />
+          </Routes>
+        </Container>
+      </Router>    
+    </WeatherProvider>
   </React.StrictMode>
 );
 
