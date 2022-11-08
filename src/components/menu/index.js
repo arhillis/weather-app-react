09bd3819bab887edf72d14902c3a00ff3e71f7d1
Andrielle Hillis <andriellerhillis@gmail.com>
2022-11-08 10:43:06 -0600

    Redid navbar

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,27 +1,44 @@
 import { Link } from 'react-router-dom';
+import { useWeatherContext } from '../../context/weather-context';
 
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavDropdown } from 'react-bootstrap';
 
 function Menu(){
-    return (<Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+    const {getCurrentLocation, showModal} = useWeatherContext();
+    
+    return (<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed='top'>
                 <Container>
-                    <Navbar.Brand> 
-                            <Link to='/' className='nav-link'>
-                                Weather app
-                            </Link>
-                    </Navbar.Brand>
-                    <Nav>
+                    <Navbar.Brand href="#home">
                         <Link to='/' className='nav-link'>
-                            Home
+                            Weather app
                         </Link>
-                        <Link to='/daily-forecast' className='nav-link'>
-                            Daily Forecast
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link to='/' className='nav-link'>
+                            Current Weather
                         </Link>
+                        <NavDropdown title="Forecast" id="collasible-nav-dropdown">
+                            <Link to='/daily-forecast' className='dropdown-item'>
+                                Daily
+                            </Link>
+                        <NavDropdown.Item href="#action/3.2">
+                            Hourly
+                        </NavDropdown.Item>
+                        </NavDropdown>
                     </Nav>
+                    <Nav>
+                        <Button variant="outline-light" size="sm" onClick={getCurrentLocation} className='mx-1'>Current Location</Button>
+                        <Button variant="outline-light" size="sm" onClick={showModal}>Change Location</Button>
+                    </Nav>
+                    </Navbar.Collapse>
                 </Container>
-    </Navbar>)
+                </Navbar>)
 }
 
 export default Menu;
