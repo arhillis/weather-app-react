21245539296b6f46ed7d26b168e7ce3b7ec14537
Andrielle Hillis <andriellerhillis@gmail.com>
2022-11-09 09:46:50 -0600

    Got rid of Geo api url and key

--- src/api.js
+++ src/api.js
@@ -1,14 +1,3 @@
-//Used in the search component
-export const geoApiOptions = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_GEO_API_KEY,// enter your rapid api key here
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-  }
-};
-
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
-
 //Used in App.js (showPosition)
 export const MAPBOX_API_KEY = process.env.REACT_APP_MAPBOX_API_KEY;
 
