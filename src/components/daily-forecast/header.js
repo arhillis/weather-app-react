a799bcec394091fa2606ac982a4c61b2d4e1f137
Andrielle Hillis <andriellerhillis@gmail.com>
2022-11-09 13:12:33 -0600

    Daily forecast component finished

---
+++ src/components/daily-forecast/header.js
@@ -0,0 +1,28 @@
+function Header(props){
+    const {day: {
+            dt,
+            temp: {max, min},
+            weather: [desc]
+        },
+        degUnit
+    } = props;
+
+    const {description, icon} = desc;
+    const dateObj = new Date(dt * 1000);
+
+    return (<>
+                 <div className="day">
+                     {dateObj.toDateString().slice(0, 10)}
+                 </div>
+                 <div>{Math.round(max)}&deg;{degUnit} / {Math.round(min)}&deg;{degUnit}</div>                  
+                 <div>
+                     <img alt="Weather icon" 
+                         src={`icons/${icon}.png`} 
+                         width='30'
+                     />
+                         {description}
+                     </div>
+            </>)
+}
+
+export default Header;
--- src/components/daily-forecast/index.js
+++ src/components/daily-forecast/index.js
@@ -1,5 +1,34 @@
+import {useWeatherContext} from '../../context/weather-context';
+import './daily-forecast.scss';
+import { Accordion} from 'react-bootstrap';
+
+import Body from './body';
+import Header from './header';
+
 function DailyForecast(){
-    return (<div>Daily forecast goes here...</div>)
+    const {dailyForecast}= useWeatherContext();
+
+    if(!dailyForecast){
+        return (<>Loading data...</>)
+    }
+
+    const {daily, currentCity, degUnit} = dailyForecast;
+    
+    return (<>
+        <h1 className='py-3'>{currentCity}</h1>
+        <Accordion>
+            {daily.map((day, index )=> (
+                <Accordion.Item eventKey={index} key={index}>
+                    <Accordion.Header>
+                        <Header day={day} degUnit={degUnit}/>
+                    </Accordion.Header>
+                    <Accordion.Body>
+                        <Body day={day} degUnit={degUnit} />
+                    </Accordion.Body>
+                </Accordion.Item>
+            ))}
+        </Accordion>
+    </>)
 }
 
 export default DailyForecast;
