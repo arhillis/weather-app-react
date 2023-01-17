aa3cff6e6458b7ac9034f37d406bca6019358618
Andrielle Hillis <andriellerhillis@gmail.com>
2023-01-17 16:01:01 -0600

    Holding off on fetching weather data

--- src/context/weather-context.js
+++ src/context/weather-context.js
@@ -24,27 +24,36 @@ const WeatherProvider = ({children}) =>{
         const [latitude, longitude] = searchData.value.split(' ');
         const degUnit = unit === 'imperial' ? 'F' : 'C';
         const lastCall = JSON.parse(localStorage.getItem('lastCall'));
-        const {lastCallTime} = lastCall;
+        const {lastCallTime, currentCity} = lastCall;
         const currentTime = Date.now();
         const timeElapsed = currentTime - lastCallTime;
 
         /**         * 
-         * if there is a last call, and if it has been less than five minutes since the last call, fetch data local storage
+         * if there is a last call, and if it has been less than five minutes since the last call, 
+         * fetch data local storage if:
+         *      - there is a last call
+         *      - it has been less than five minutes since the last call
+         *      - there is a currentCity
+         *      - the current city and the label from searchData are the same
          * else fetch data from the api
          * 1000 ms in a sec, 60 secs in 1 min, 5 minutes = 60,000 * 3 = 180,000
+         * 
+         * label - from searchData
+         * location - from useState
+         * 
          */
-        if(lastCallTime && timeElapsed > 300000){
-            const {current, daily, hourly} = lastCall;
-            setCurrentWeather({currentCity: label, latitude, longitude, degUnit,...current});
-            setDailyForecast({currentCity: label, daily, degUnit});
-            setHourlyForecast({currentCity: label, hourly, degUnit});
+        if(lastCallTime && timeElapsed < 300000 && currentCity && currentCity === label){
+            const {current, daily, hourly, currentCity} = lastCall;
+            setCurrentWeather({currentCity, latitude, longitude, degUnit,...current});
+            setDailyForecast({currentCity, daily, degUnit});
+            setHourlyForecast({currentCity, hourly, degUnit});
         }else{
             const oneCall = await getWeatherData('onecall', unit, latitude, longitude); 
             const {current, daily, hourly} = oneCall;
             setCurrentWeather({currentCity: label, latitude, longitude, degUnit,...current});
             setDailyForecast({currentCity: label, daily, degUnit});
             setHourlyForecast({currentCity: label, hourly, degUnit});
-            localStorage.setItem('lastCall', JSON.stringify({lastCallTime: currentTime, current, daily, hourly, unit}))
+            localStorage.setItem('lastCall', JSON.stringify({lastCallTime: currentTime, current, daily, hourly, unit, currentCity: label}))
         }
 
     }
@@ -54,11 +63,12 @@ const WeatherProvider = ({children}) =>{
             fetch(`${MAPBOX_API_URL}/mapbox.places/${longitude},${latitude}.json?access_token=${MAPBOX_API_KEY}`)
             .then(res => res.json())
             .then(data =>{ 
-                const {context} =data.features[0];
-                handleSearchChange({
-                    label: `${context[1].text}, ${context[3].text}`, 
-                    value: `${latitude} ${longitude}`
-                });
+                console.log(data);
+                // const {context} =data.features[0];
+                // handleSearchChange({
+                //     label: `${context[1].text}, ${context[3].text}`, 
+                //     value: `${latitude} ${longitude}`
+                // });
             })
             .catch(err => console.log(err))
         }
