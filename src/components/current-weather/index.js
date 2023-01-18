ae62c41bbfa103ef7e43cc0180c78e2a9eda8378
Andrielle Hillis <andriellerhillis@gmail.com>
2023-01-17 20:25:45 -0600

    Got it down to the bare bones

--- src/components/current-weather/index.js
+++ src/components/current-weather/index.js
@@ -1,71 +1,73 @@
 import PropTypes from 'prop-types';
 import './current-weather.scss';
-import { useWeatherContext } from '../../context/weather-context'; 
+// import { useWeatherContext } from '../../context/weather-context'; 
 
-import Card from 'react-bootstrap/Card';
-import ListGroup  from 'react-bootstrap/ListGroup';
+// import Card from 'react-bootstrap/Card';
+// import ListGroup  from 'react-bootstrap/ListGroup';
 
 const CurrentWeather = () =>{
-    const {currentWeather} = useWeatherContext();
+    // const {currentWeather} = useWeatherContext();
 
-    if(!currentWeather){
-        return (<div className=' mx-auto mt-3'>
-            Loading data...
-        </div>)
-    }
+    // if(!currentWeather){
+    //     return (<div className=' mx-auto mt-3'>
+    //         Loading data...
+    //     </div>)
+    // }
 
-    const {
-        currentCity, dt, feels_like, humidity, pressure, temp, degUnit, wind_speed,
-        weather: [{description, icon}],
-    } = currentWeather;
-    const dateObj = new Date(dt * 1000);
+    // const {
+    //     currentCity, dt, feels_like, humidity, pressure, temp, degUnit, wind_speed,
+    //     weather: [{description, icon}],
+    // } = currentWeather;
+    // const dateObj = new Date(dt * 1000);
 
-    return (<><h1 className='py-3'>{currentCity}</h1>
-                <h2>Current Weather</h2>
-            <Card className=' mx-auto mt-3'>
-                    <Card.Body className='row'>  
-                        <div className="date-time col-6">
-                            <p className='time'>Local Date and Time: </p>                                                       
-                        </div>     
-                        <div className="date-time col-6">
-                            {dateObj.toLocaleString('en-us', {
-                                                        weekday: 'long',
-                                                        month: 'short',
-                                                        day: 'numeric',
-                                                        hour: 'numeric',
-                                                        minute: '2-digit'
-                                                    })}
-                        </div>
-                        <div className="temperature col-6">
-                            {Math.round(temp)} &deg;{degUnit}
-                            <div className='icon'>
-                                <Card.Img variant="top" src={`icons/${icon}.png`} />
-                                <p className='weather-description'>{description}</p>
-                            </div>
-                        </div>
-                        <div className="details col-6">
-                            <h5>Details</h5>  
-                            <ListGroup variant='flush'>
-                                <ListGroup.Item>
-                                    <span>Feels Like</span>
-                                    <span>{Math.round(feels_like)} &deg;{degUnit}</span>
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <span >Wind</span>
-                                    <span>{Math.round(wind_speed)} m/s</span>
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <span>Humidity</span>
-                                    <span>{humidity}%</span>
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <span>Pressure</span>
-                                    <span>{pressure} hPa</span>
-                                </ListGroup.Item>
-                            </ListGroup> 
-                        </div>
-                     </Card.Body>
-                </Card></>)
+    return (<>Current weather goes here...</>)
+
+    // return (<><h1 className='py-3'>{currentCity}</h1>
+    //             <h2>Current Weather</h2>
+    //         <Card className=' mx-auto mt-3'>
+    //                 <Card.Body className='row'>  
+    //                     <div className="date-time col-6">
+    //                         <p className='time'>Local Date and Time: </p>                                                       
+    //                     </div>     
+    //                     <div className="date-time col-6">
+    //                         {dateObj.toLocaleString('en-us', {
+    //                                                     weekday: 'long',
+    //                                                     month: 'short',
+    //                                                     day: 'numeric',
+    //                                                     hour: 'numeric',
+    //                                                     minute: '2-digit'
+    //                                                 })}
+    //                     </div>
+    //                     <div className="temperature col-6">
+    //                         {Math.round(temp)} &deg;{degUnit}
+    //                         <div className='icon'>
+    //                             <Card.Img variant="top" src={`icons/${icon}.png`} />
+    //                             <p className='weather-description'>{description}</p>
+    //                         </div>
+    //                     </div>
+    //                     <div className="details col-6">
+    //                         <h5>Details</h5>  
+    //                         <ListGroup variant='flush'>
+    //                             <ListGroup.Item>
+    //                                 <span>Feels Like</span>
+    //                                 <span>{Math.round(feels_like)} &deg;{degUnit}</span>
+    //                             </ListGroup.Item>
+    //                             <ListGroup.Item>
+    //                                 <span >Wind</span>
+    //                                 <span>{Math.round(wind_speed)} m/s</span>
+    //                             </ListGroup.Item>
+    //                             <ListGroup.Item>
+    //                                 <span>Humidity</span>
+    //                                 <span>{humidity}%</span>
+    //                             </ListGroup.Item>
+    //                             <ListGroup.Item>
+    //                                 <span>Pressure</span>
+    //                                 <span>{pressure} hPa</span>
+    //                             </ListGroup.Item>
+    //                         </ListGroup> 
+    //                     </div>
+    //                  </Card.Body>
+    //             </Card></>)
    
 }
 
